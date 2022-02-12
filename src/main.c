f2522b06b40d1326a77193c0fc726cb722c966e4
Paul Aiton <paul@aiton.info>
2022-02-11 22:33:07 -0700

    menu outline

--- src/main.c
+++ src/main.c
@@ -1,3 +1,61 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void heap_menu(){
+    printf("HEAP BREAK MENU\n");
+    printf("#######################################");
+    printf("\n\n");
+    return;
+}
+
+void anon_mmap_menu(){
+    printf("ANONYMOUS MMAP MENU\n");
+    printf("#######################################");
+    printf("\n\n");
+    return;
+}
+
+void file_mmap_menu(){
+    printf("FILE MMAP MENU\n");
+    printf("#######################################");
+    printf("\n\n");
+    return;
+}
+
+short main_menu(){
+    printf("MAIN MENU\n");
+    printf("#######################################");
+    printf("\n\n");
+    printf(" 1  :  Change heap break \n");
+    printf(" 2  :  Anonymous mmap \n");
+    printf(" 3  :  File mmap \n");
+    printf(" 0  :  Exit \n");
+    printf("\nSelection : ");
+    int selection = getchar();
+    getchar();
+    printf("\n");
+    return selection;
+}
 
 int main (int argc, const char *argv[]){
+    short main_menu_selection = 0;
+    while(1){
+        main_menu_selection = main_menu();
+        switch(main_menu_selection){
+            case '0' :
+                exit(0);
+                break;
+            case '1' :
+                heap_menu();
+                break;
+            case '2' :
+                anon_mmap_menu();
+                break;
+            case '3' :
+                file_mmap_menu();
+                break;
+        }
+
+    }
 }
+
