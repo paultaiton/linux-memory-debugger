9b0d4888976e56e3b387bbe91a7b7cbe3bd449a6
Paul Aiton <paul@aiton.info>
2025-02-09 12:42:12 -0700

    init and print test

--- makefile
+++ makefile
@@ -13,7 +13,7 @@ lib:
 ####################################################
 
 bin/linux-memory-debugger: src/main.c bin
-	gcc ${CFLAGS} -o bin/linux-memory-debugger src/main.c -lncurses
+	gcc ${CFLAGS} -o bin/linux-memory-debugger src/main.c -lncurses -ltinfo
 	# strip bin/linux-memory-debugger
 
 ####################################################
--- src/main.c
+++ src/main.c
@@ -1,23 +1,23 @@
 #include <curses.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+const char * hello_string = "Hello World!";
 
 int main (int argc, const char *argv[]){
     initscr();
     cbreak();
     noecho();
 
-    mvaddch(0, 0, 'H');
-    addch('e');
-    addch('l');
-    addch('l');
-    addch('o');
-    addch(' ');
-    addch('W');
-    addch('o');
-    addch('r');
-    addch('l');
-    addch('d');
-    addch('!');
+    move(0, 0);
+    for (int i=0; i < strlen(hello_string); i++) {
+        addch(hello_string[i]);
+    }
+    refresh();
+
+    getchar();
+
+    endwin();
 }
 
