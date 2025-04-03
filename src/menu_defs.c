0fb009fe57d1efa573a7df63b11b591ef5318c03
Paul Aiton <paul@aiton.info>
2025-04-02 18:48:43 -0700

    .

---
+++ src/menu_defs.c
@@ -0,0 +1,16 @@
+const static char *main_menu_choices[] = {
+	"Choice 1",
+	"Choice 2",
+	"Choice 3",
+	"Choice 4",
+	"Exit",
+	};
+
+// const static char *main_menu_choices[] = {
+	// "Choice 1",
+	// "Choice 2",
+	// "Choice 3",
+	// "Choice 4",
+	// "Exit",
+	// };
+
