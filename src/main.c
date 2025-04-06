6c576dd88f5e778cc992766758bcd326714e74f3
Paul Aiton <paul@aiton.info>
2025-04-06 16:10:06 -0700

    working dynamically

--- src/main.c
+++ src/main.c
@@ -7,14 +7,7 @@
 
 #include "menu_defs.c"
 
-#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
-
-//ITEM *main_menu_items[ARRAY_SIZE(main_menu_choices) + 1]; //= (ITEM**)calloc(number_of_choices +1, sizeof(ITEM *));
-//ITEM *main_menu_items[6]; //= (ITEM**)calloc(number_of_choices +1, sizeof(ITEM *));
-// ITEM *main_menu_items[ARRAY_SIZE(main_menu_choices) + 1]; //= (ITEM**)calloc(number_of_choices +1, sizeof(ITEM *));
-
 int main (int argc, const char *argv[]){
-	static ITEM *main_menu_items[ARRAY_SIZE(main_menu_choices) + 1];
 	initscr();
 	cbreak();
 	noecho();
@@ -26,56 +19,15 @@ int main (int argc, const char *argv[]){
 	mvprintw(22, 0, "Columns: %u", COLS );
 	mvprintw(23, 0, "Lines: %u", LINES);
 
-	int number_of_choices = ARRAY_SIZE(main_menu_choices);
-	for(int i=0; main_menu_choices[i] != NULL; i++) {
-		main_menu_items[i] = new_item(main_menu_choices[i]->name, main_menu_choices[i]->descritpion);
-		if (main_menu_items[i]== NULL) {
-			perror("ERROR E001: failure while instantiating menu_items.\n");
-			endwin();
-			exit(1);
-		}
-	}
-	main_menu_items[number_of_choices] = (ITEM *)NULL;
-
-	MENU *main_menu = new_menu(main_menu_items);
-	post_menu(main_menu);
 
 	refresh();
 
-	static int key;
-	static int quit = 0;
-	ITEM *selected_item = current_item(main_menu);
-	while(quit == 0){
-		selected_item = current_item(main_menu);
-		mvprintw(8, 0, "Selected item is %p", selected_item);
-		mvprintw(9, 0, "Exit item is %p", main_menu_items[4]);
-		key = getch();
-		move(10, 0);
-		clrtoeol();
-		printw("Input was %u, %s", key, keyname(key));
-		mvprintw(11, 0, "Enter is %u, %x", KEY_ENTER, KEY_ENTER );
-		switch(key) {
-			case KEY_DOWN:
-				menu_driver(main_menu, REQ_DOWN_ITEM);
-				break;
-			case KEY_UP:
-				menu_driver(main_menu, REQ_UP_ITEM);
-				break;
-			//case KEY_RIGHT:
-			case KEY_RIGHT:
-				//if (selected_item == (ITEM*)main_menu_choices[4]) {
-				if (selected_item == main_menu_items[4]) {
-					quit = 1;
-				}
-				break;
-		}
-	}
+	call_main_menu();
 
-	unpost_menu(main_menu);
-
-	free_menu(main_menu);
-	for(int i=0; i < number_of_choices; i++) {
-		free_item(main_menu_items[i]);
-	}
+	// free_menu(main_menu);
+	// for(int i=0; i < number_of_choices; i++) {
+	// 	free_item(main_menu_items[i]);
+	// }
 	endwin();
 }
+
--- src/menu_defs.c
+++ src/menu_defs.c
@@ -1,11 +1,22 @@
 #include <curses.h>
+#include <menu.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+
+static int quit_flag = 0;
+int success() {
+	return TRUE;
+}
 int failure() {
 	return FALSE;
 }
-
-int success() {
-	return TRUE;
+int quit() {
+	quit_flag = 1;
+	return 0;
 }
 
 struct menu_item {
@@ -22,21 +33,62 @@ struct menu_item choice1 = {
 	.curs_item = NULL
 };
 
-//const static char *pause_message = "Press F1 key to exit...";
-static char *pause_message = "Hello";
+struct menu_item main_menu_quit = {
+	.name = "EXIT",
+	.descritpion = "quit the program",
+	.invoker = quit,
+	.curs_item = NULL
+};
 
 const static struct menu_item *main_menu_choices[] = {
 	&choice1,
-	NULL,
-	NULL,
+	&main_menu_quit,
 	NULL,
 	};
 
+int call_main_menu() {
+	static MENU* main_menu = NULL;
+	static ITEM *main_menu_items[ARRAY_SIZE(main_menu_choices) + 1];
+	if (main_menu == NULL) {
+		for(int i=0; main_menu_choices[i] != NULL; i++) {
+			main_menu_items[i] = new_item(main_menu_choices[i]->name, main_menu_choices[i]->descritpion);
+			if (main_menu_items[i]== NULL) {
+				perror("ERROR E001: failure while instantiating menu_items.\n");
+				endwin();
+				exit(1);
+			}
+		}
+		main_menu = new_menu(main_menu_items);
+	}
+	post_menu(main_menu);
 
-// const static char *main_menu_choices[] = {
-// 	"Choice 1",
-// 	"Choice 2",
-// 	"Choice 3",
-// 	"Choice 4",
-// 	"Exit",
-// 	};
+	int key;
+	ITEM *selected_item;
+	while(quit_flag == 0){
+		selected_item = current_item(main_menu);
+		mvprintw(8, 0, "Selected item is %p", selected_item);
+		mvprintw(9, 0, "Exit item is %p", main_menu_items[1]); // TODO This is bad, don't use index.
+		key = getch();
+		move(10, 0);
+		clrtoeol();
+		printw("Input was %u, 0x%x, %s", key, key, keyname(key));
+		mvprintw(11, 0, "Enter is %u, 0x%x", KEY_ENTER, KEY_ENTER );
+		switch(key) {
+			case KEY_DOWN:
+				menu_driver(main_menu, REQ_DOWN_ITEM);
+				break;
+			case KEY_UP:
+				menu_driver(main_menu, REQ_UP_ITEM);
+				break;
+			case KEY_RIGHT:
+				main_menu_choices[item_index(current_item(main_menu))]->invoker();
+				break;
+		}
+	}
+	unpost_menu(main_menu);
+	return 0;
+}
+
+//const static char *pause_message = "Press F1 key to exit...";
+static char *pause_message = "Hello";
+
